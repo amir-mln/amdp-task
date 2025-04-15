c382410f6fe936b7fa32df9a47223d6a3b81d8b1
amir-mln <molaeyan.career@gmail.com>
2025-04-15 14:35:08 +0330

    fix: bus package pointer type

--- system/core/bus/utils.go
+++ system/core/bus/utils.go
@@ -6,6 +6,10 @@ import (
 )
 
 func getRegistryKey(t reflect.Type) (string, error) {
+	if t.Kind() == reflect.Pointer {
+		t = t.Elem()
+	}
+
 	name, pkgPath := t.Name(), t.PkgPath()
 	if name == "" || pkgPath == "" {
 		return "", fmt.Errorf("empty type name or package path, name:%q, pkg:%q", name, pkgPath)
